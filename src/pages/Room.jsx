a9b3fdb31594fbc9eb7746fe5f3d0c83d5ed6274
Shafi Rayhan <rayhanshafi7@gmail.com>
2023-05-13 23:04:51 +0600

    fixed bottom input

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -44,23 +44,30 @@ export default function Room() {
 
   return (
     <>
-      <main>
+      <main className="flex flex-col-reverse gap-4 p-4">
         {messages &&
           messages.map((msg, index) => <Message key={index} message={msg} />)}
-
         <span ref={dummy} />
       </main>
 
-      <form onSubmit={sendMessage}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="Type message"
-        />
-
-        <button type="submit" disabled={!formValue}>
-          ☄️
-        </button>
+      <form className="fixed bottom-0 left-0 w-full" onSubmit={sendMessage}>
+        <div className="relative">
+          <input
+            type="text"
+            className="block w-full py-4 pl-2 pr-16 text-sm text-gray-900 border border-gray-300 bg-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Type message..."
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="text-white absolute right-0 bottom-0 top-0 bg-gray-900 hover:bg-gray-700 focus:outline-none font-medium text-sm px-4 py-2"
+            disabled={!formValue}
+          >
+            Send
+          </button>
+        </div>
       </form>
     </>
   );
