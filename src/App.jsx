3ddc815c11494f93be6263a52df21e6b448b1b97
Shafi Rayhan <rayhanshafi7@gmail.com>
2023-05-13 16:23:43 +0600

    app root

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/firebase';
+
+import Login from './pages/Login';
+import Room from './pages/Room';
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return <div className="App">{user ? <Room /> : <Login />}</div>;
+}
+
+export default App;
