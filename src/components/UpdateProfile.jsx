c85dc85beb96a803c3be3d7093d14ca12463c565
Shafi Rayhan <rayhanshafi7@gmail.com>
2023-05-14 00:00:33 +0600

    handle profile update

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,4 +1,25 @@
-export function UpdateProfile() {
+import { updateProfile } from 'firebase/auth';
+import { useState } from 'react';
+
+export function UpdateProfile({ user, close }) {
+  const [name, setName] = useState(user.displayName);
+
+  const update = (e) => {
+    e.preventDefault();
+
+    updateProfile(user, {
+      displayName: name,
+      photoURL: `https://api.dicebear.com/6.x/pixel-art/svg?seed=${name
+        .split(' ')
+        .join('+')}`,
+    }).then((r) => {
+      console.log(r);
+    });
+
+    localStorage.setItem('showProfileModal', true);
+    close(false);
+  };
+
   return (
     <div className="fixed inset-0 z-40 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
       <div tabIndex="-1" aria-hidden="true" className="z-50 w-full p-4">
@@ -8,6 +29,7 @@ export function UpdateProfile() {
               type="button"
               className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
               data-modal-hide="authentication-modal"
+              onClick={() => close(false)}
             >
               <svg
                 aria-hidden="true"
@@ -29,10 +51,9 @@ export function UpdateProfile() {
                 Update Your Profile
               </h3>
               <p className="text-sm text-gray-400 mb-4">
-                Pick a username and a profile image that would be displayed to
-                other users.
+                Pick a username that would be displayed to other users.
               </p>
-              <form className="space-y-6" action="#">
+              <form className="space-y-6" onSubmit={update}>
                 <div>
                   <label
                     htmlFor="name"
@@ -44,28 +65,16 @@ export function UpdateProfile() {
                     type="text"
                     id="name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="Shefuda"
+                    placeholder="Hero Alom"
                     required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="image"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Image URL
-                  </label>
-                  <input
-                    type="text"
-                    id="image"
-                    placeholder="https://i.pravatar.cc/150?img=3"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
                 <button
                   type="submit"
                   className="w-full text-white bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-gray-900 dark:focus:ring-blue-800"
+                  disabled={!name}
                 >
                   Update Profile
                 </button>
