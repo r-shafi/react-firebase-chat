0f3625f6844a5c31c5e33e571b49d62bdddfbc82
Shafi Rayhan <rayhanshafi7@gmail.com>
2023-05-13 23:08:47 +0600

    update message component styles

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,37 @@
 import { auth } from '../firebase/firebase';
 
 export function Message(props) {
-  const { text, uid, photoURL, name } = props.message;
+  const { text, uid, photoURL, name, createdAt } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const sent = uid === auth.currentUser.uid;
 
   return (
-    <div>
-      <div className={`message ${messageClass}`}>
-        <img src={photoURL} alt="" />
-        <p className="text messageBubble">
-          <b className="nameTag">{`${name} Says:`}</b>
-          <br /> {text}
-        </p>
+    <div
+      className={`w-full max-w-xs p-4 text-gray-500 ${
+        sent ? 'bg-red-200 self-end' : 'bg-green-200'
+      } rounded-lg shadow dark:bg-gray-800 dark:text-gray-400`}
+    >
+      <div className="flex">
+        <img
+          className="w-8 h-8 rounded-full shadow-lg"
+          src={photoURL}
+          alt={name}
+        />
+        <div className="ml-3 text-sm font-normal">
+          <span className="mb-1 text-sm font-semibold text-gray-900 flex flex-col">
+            {name}
+            <span className="text-xs font-light text-gray-500">
+              {createdAt?.toDate().toLocaleTimeString()}
+              {' -- '}
+              {createdAt?.toDate().toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </span>
+          </span>
+          <p className="mb-2 text-sm font-normal text-gray-900">{text}</p>
+        </div>
       </div>
     </div>
   );
