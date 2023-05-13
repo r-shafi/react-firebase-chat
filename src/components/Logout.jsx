d0b56e82edff7bfc11f3d0e5d4fc24ffc3304912
Shafi Rayhan <rayhanshafi7@gmail.com>
2023-05-13 16:21:56 +0600

    logout component

---
+++ src/components/Logout.jsx
@@ -0,0 +1,11 @@
+import { auth } from '../firebase/firebase';
+
+export function Logout() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
